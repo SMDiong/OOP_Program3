19f623ca9c7ae893b8640389008b8f5a193cfc41
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-03-24 02:29:22 +0800

    Added input variables

--- oop_project3.py
+++ oop_project3.py
@@ -3,19 +3,30 @@
 # Project 3
 
 # Word Formatting for Fancy Words
+from art import *
 
 # This will ask the user what's their name
+user_name = input("Please enter your name: ")
 
 # This will ask the user what's their age
+age = input("Please enter your age: ")
 
 # This will ask the user what's their sex
+sex = input("Please enter your sex: ")
 
 # This will ask the user what's their dream job
+dream_job = input("Please enter your dream job: ")
 
 # This will ask the user what's their email address
+email_address = input("Please enter your email address: ")
 
 # This will ask the user what's their current city
+user_city = input("Please enter your city: ")
 
 # This will ask the user what's their school
+user_school = input("Please enter your school: ")
 
-# This will askt he user what's their course
+# This will ask the user what's their course
+user_course = input("Please enter your course: ")
+
+# Print inputs from the user
