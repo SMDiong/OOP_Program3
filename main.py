db212255cd4f7658da3d18a9cfccab8600cc25f8
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-03-24 02:17:14 +0800

    Added Pseudocode

---
+++ main.py
@@ -0,0 +1,19 @@
+# Diong, Shan Marc C.
+# March 23, 2024
+# Project 3
+
+# This will ask the user what's their name
+
+# This will ask the user what's their age
+
+# This will ask the user what's their sex
+
+# This will ask the user what's their dream job
+
+# This will ask the user what's their email address
+
+# This will ask the user what's their current city
+
+# This will ask the user what's their school
+
+# This will askt he user what's their course
